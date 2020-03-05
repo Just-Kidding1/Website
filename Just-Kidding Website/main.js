d129ae69ba4a44c43e0d04ced5f60710a0f04522
Jason Duncan <59481139+Dragon4000@users.noreply.github.com>
2020-03-05 09:56:52 -0600

    Add files via upload

---
+++ Just-Kidding Website/main.js
@@ -0,0 +1,7 @@
+function Button(){
+    let button = document.getElementById("button0");
+firstName =  Jason ;
+companyName =  Neutral ;
+alert(`${firstName} works at ${companyName}.`);
+//returns “Michael works at Dunder Mifflin.”
+}
