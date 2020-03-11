b96f21bf0ba062c1350d7aa7b4d6fc93494fbbbe
Jason Duncan <59481139+Dragon4000@users.noreply.github.com>
2020-03-11 10:11:36 -0500

    Add files via upload

--- Just-Kidding Website/main.js
+++ Just-Kidding Website/main.js
@@ -1,7 +1,7 @@
 function Button(){
-    let button = document.getElementById("button0");
-    Name1 =  'Jason' ;
-    Name2 =  'Kevin' ;
-    alert(`this was created by ${Name1} and ${Name2}.`);
-//returns “Jason works for Dunder Neutral.”
+    let button = document.getElementById("button0")
+
+    num1 = 4;
+    num2 = .5;
+    alert(num1 + num2);
 }
